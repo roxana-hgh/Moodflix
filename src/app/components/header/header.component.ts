3166a6bb5cb3a39166387c1c62e3e650e8548168
roxana-hgh <haghgoo.roxana@gmail.com>
2025-04-28 16:37:41 +0330

    header added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isScrolled = false;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      window.addEventListener('scroll', () => {
+        this.isScrolled = window.scrollY > 0;
+      });
+    }
+  }
 }
